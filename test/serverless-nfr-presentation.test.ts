43ef98f4b7ffecfb2a61ea68de8eb7254590585d
Rob Bulmer <r95@hotmail.co.uk>
2024-05-29 13:29:58 +0100

    Initial commit

---
+++ test/serverless-nfr-presentation.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessNfrPresentation from '../lib/serverless-nfr-presentation-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-nfr-presentation-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessNfrPresentation.ServerlessNfrPresentationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
